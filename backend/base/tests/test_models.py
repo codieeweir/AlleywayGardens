75d17a2349812815e6dcfbff20d214062826a0e8
Courtney Weir <courtneyweir14@gmail.com>
2025-01-19 21:13:43 +0000

    test_Models.py file update with base django testing framework and deletion of tests.py file

--- backend/base/tests/test_models.py
+++ backend/base/tests/test_models.py
@@ -1,25 +1,83 @@
 import pytest
+from django.test import TestCase
+from django.core.exceptions import ValidationError
+from django.contrib.gis.geos import Point
+from .factories import ProjectFactory, ZoneFactory, UserFactory, MessageFactory
+from base.models import Message
+from django.db.utils import DataError
 
 pytestmark = pytest.mark.django_db
 
-# Zone Testing
-class TestZoneModel: 
-    def test_str_return(self, zone_factory):
-        zone = zone_factory(name="testingZone")
-        assert zone.__str__() == "testingZone"
+#Base Django testing to start... incl factories
 
 # Project Testing
-class TestProjectModel: 
-    def test_str_return(self, project_factory):
-        project = project_factory(name="testingProjectName")
-        assert project.__str__() == "testingProjectName"
+class TestProjectModel(TestCase): 
+    def test_str_return(self):
+        project = ProjectFactory(name="testingProjectName")
+        self.assertEqual(str(project), "testingProjectName")
 
+    def test_default_location(self):
+        project = ProjectFactory()
+        self.assertEqual(project.location.coords,(-5.93012, 54.5973))
+
+    def test_max_legth_name(self):
+        project = ProjectFactory(name = "A" * 200)
+        self.assertEqual(len(project.name), 200)
+        with self.assertRaises(DataError):
+            ProjectFactory(name = "A" * 201)
+
+    def test_foreign_key_delete_set_null(self):
+        host = UserFactory()
+        project = ProjectFactory(host = host)
+        host.delete()
+        project.refresh_from_db()
+        self.assertIsNone(project.host)
+
+    def test_many_to_many_relationship(self):
+        project = ProjectFactory()
+        user = UserFactory()
+        project.participants.add(user)
+        self.assertIn(user, project.participants.all())
+
+    def test_value_of_srid_for_location(self):
+        project = ProjectFactory()
+        self.assertEqual(project.location.srid, 4326)
+
+
+# Zone Testing
+class TestZoneModel(TestCase):
+    def test_str_return(self):
+        zone = ZoneFactory(name="testingZone")
+        self.assertEqual(str(zone), "testingZone")
+
+    def test_max_legth_name(self):
+        zone = ZoneFactory(name = "A" * 200)
+        self.assertEqual(len(zone.name), 200)
+        with self.assertRaises(DataError):
+            ZoneFactory(name = "A" * 201)
 
 # Message Testing
-class TestMessageModel: 
-    def test_str_return(self, message_factory):
-        message = message_factory(body="testingMessage")
-        assert message.__str__() == "testingMessage"
+class TestMessageModel(TestCase): 
+    def test_str_return(self):
+        message = MessageFactory(body="testingMessage")
+        self.assertEqual(str(message), "testingMessage")
+
+    def test_on_delete_cascade(self):
+        user = UserFactory()
+        project = ProjectFactory(host=user)
+        
+        message1 = MessageFactory(user=user, project=project)
+        message2 = MessageFactory(user=user, project=project)
+
+        self.assertIn(message1, Message.objects.all())
+        self.assertIn(message2, Message.objects.all())
+
+        user.delete()
+
+        self.assertNotIn(message1, Message.objects.all())
+        self.assertNotIn(message2, Message.objects.all())
+
+# Trying out some pytest 
 
 
 # Post Tetsing
